165605f671423aa9d3de871f775936cd08a7d5ad
Brandon Williams <bwill904@gmail.com>
2022-12-04 14:02:21 -0500

    #2 Adjusting timezone of historical load data

--- PythonCode/HistoricalLoadDataSPP.py
+++ PythonCode/HistoricalLoadDataSPP.py
@@ -1,6 +1,7 @@
 # Meant to be ran once, grabbing long term historical data from SPP
 import pandas as pd
 import os
+from datetime import timedelta
 
 strHistLoadDir = 'C:/Users/bwill/Documents/RainShine/Data/Load/History/SPP'
 for year in os.listdir(strHistLoadDir):
@@ -11,5 +12,6 @@ for year in os.listdir(strHistLoadDir):
             dfHistLoad = dfTemp
         else: dfHistLoad = pd.concat([dfHistLoad, dfTemp])
 
-dfHistLoad['MarketHour'] = pd.to_datetime(dfHistLoad['MarketHour'])
+dfHistLoad['MarketHour'] = pd.to_datetime(dfHistLoad['MarketHour']) - timedelta(hours=7) # convert from GMT to CST, get data in hour beginning
+dfHistLoad = dfHistLoad.round(decimals=2)
 dfHistLoad.to_csv('C:/Users/bwill/Documents/RainShine/Data/Load/History/HistLoadSPP.csv', index=False)
--- PythonCode/UpdateLoadDataSPP.py
+++ PythonCode/UpdateLoadDataSPP.py
@@ -1,7 +1,7 @@
 # Update historical SPP load data
 
 from datetime import date
-import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 
 dfHistLoad = pd.read_csv('C:/Users/bwill/Documents/RainShine/Data/Load/History/HistLoadSPP.csv')
@@ -12,7 +12,7 @@ for day in pd.date_range(lastDay.strftime("%m/%d/%Y"), date.today().strftime("%m
     try:
         dfTemp = pd.read_csv('https://marketplace.spp.org/file-browser-api/download/hourly-load?path=%2F' + str(day.year) + '%2FDAILY_HOURLY_LOAD-' + str(day.strftime("%Y%m%d")) + '.csv')   
         dfTemp.columns = dfTemp.columns.str.replace(' ', '')
-        dfTemp['MarketHour'] = pd.to_datetime(dfTemp['MarketHour'])
+        dfTemp['MarketHour'] = pd.to_datetime(dfTemp['MarketHour']) - timedelta(hours=7) # convert from GMT to CST, get data in hour beginning
         dfHistLoad = pd.concat([dfHistLoad, dfTemp])
     except:
         print('Missing historical load data for ' + str(day))
