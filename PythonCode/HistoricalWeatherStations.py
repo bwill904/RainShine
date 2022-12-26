41c7016e57dec86642616af0c0edc129d0c56b19
Brandon Williams <bwill904@gmail.com>
2022-12-26 15:43:30 -0500

    #4 seperate script for weather station datapull

--- PythonCode/HistoricalWeatherStations.py
+++ PythonCode/HistoricalWeatherStations.py
@@ -4,13 +4,14 @@ import pandas as pd
 import os
 from meteostat import Stations, Point, Daily, Hourly
 
-dfStations = pd.read_csv(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Stations.csv')
-
-for station in dfStations.icao.unique():
-
-
 stations = Stations().region('US')
 dfStations = stations.fetch(10000)
 dfStations = dfStations.dropna(subset = ["region", "hourly_start", "daily_start"])
+
+# get 10 best stations for each state, may need to change this in the future
+for state in dfStations.region.unique():
+    numStations = dfStations[dfStations["region"] == state].shape[0]
+    vecRemoveStations = dfStations[dfStations["region"] == state].sort_values("hourly_start").tail(numStations - 10).icao.tolist()
+    dfStations = dfStations[~dfStations["icao"].isin(vecRemoveStations)]
 
 dfStations.to_csv(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Stations.csv', index=False)
