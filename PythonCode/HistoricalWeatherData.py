605d14d00bb82785482692a1938ee10e16d64188
Brandon Williams <bwill904@gmail.com>
2023-01-08 21:24:21 -0500

    #4 pulling hourly historical weather data

--- PythonCode/HistoricalWeatherData.py
+++ PythonCode/HistoricalWeatherData.py
@@ -2,15 +2,23 @@
 
 import pandas as pd
 import os
+import pickle
+import time
+from datetime import datetime 
 from meteostat import Stations, Point, Daily, Hourly
 
 dfStations = pd.read_csv(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Stations.csv')
 
-for station in dfStations.icao.unique():
-
-
-stations = Stations().region('US')
-dfStations = stations.fetch(10000)
-dfStations = dfStations.dropna(subset = ["region", "hourly_start", "daily_start"])
-
-dfStations.to_csv(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Stations.csv', index=False)
+for state in dfStations.region.unique():
+    dfStationsState = dfStations[dfStations['region'] == state]
+    if 'Hourly-' + state + '.pkl' in os.listdir(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History'): 
+        continue # don't write data if already there
+    else: time.sleep(20) # take a break to avoid overquerying
+    dHourly = {}
+    for stationId in dfStationsState.id.unique():
+        dfTemp = Hourly(stationId, datetime(2010, 1, 1), datetime(2022, 12, 31, 23, 59))
+        dfTemp = dfTemp.fetch()
+        dHourly[stationId] = dfTemp
+    # dHourly.to_csv(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Stations.csv', index=False)
+    with open(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Hourly-' + state + '.pkl', 'wb') as f:
+        pickle.dump(dHourly, f)
--- PythonCode/HistoricalWeatherStations.py
+++ PythonCode/HistoricalWeatherStations.py
@@ -6,12 +6,16 @@ from meteostat import Stations, Point, Daily, Hourly
 
 stations = Stations().region('US')
 dfStations = stations.fetch(10000)
+dfStations.reset_index(inplace = True)
+dfStations['id'] = dfStations['id'].astype('str')
 dfStations = dfStations.dropna(subset = ["region", "hourly_start", "daily_start"])
 
-# get 10 best stations for each state, may need to change this in the future
+dfStations = dfStations[~dfStations.region.isin(['RO', 'SA', 'WQ', 'HI', 'AK'])]
+
+# get 10 best stations, may need to change this in the future
 for state in dfStations.region.unique():
     numStations = dfStations[dfStations["region"] == state].shape[0]
     vecRemoveStations = dfStations[dfStations["region"] == state].sort_values("hourly_start").tail(numStations - 10).icao.tolist()
     dfStations = dfStations[~dfStations["icao"].isin(vecRemoveStations)]
 
-dfStations.to_csv(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Stations.csv', index=False)
+dfStations.to_csv(os.path.expanduser('~/Documents') + '/RainShine/Data/Weather/History/Stations.csv', index=False) 
